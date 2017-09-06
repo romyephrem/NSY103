bb2fbfd07b61286e144ee61510d28b162d31bfab
romyephrem <romy.ephrem@isae.edu.lb>
2017-09-06 08:23:48 +0300

    Add files via upload

---
+++ avion[1].h
@@ -0,0 +1,39 @@
+#define ALTMAX 20000
+#define ALTMIN 0
+
+#define VITMAX 1000
+#define VITMIN 200
+
+#define PAUSE 2
+
+struct coordonnees {
+  int x;
+  int y;
+  int z;
+  int altitude;
+};
+
+struct deplacement {
+  int cap;
+  int vitesse;
+};
+
+struct vitesse {
+	  int vitX;
+	  int vitY;
+	  int vitZ;
+};
+
+struct avion {
+	int id;
+	char nom;
+	char status;
+	int destX;
+	int destY;
+	int destZ;
+    struct coordonnees position;
+    struct deplacement dep;
+    struct vitesse vitesse;
+};
+
+
